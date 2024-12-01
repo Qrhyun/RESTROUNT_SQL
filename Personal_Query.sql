4706b2d0d4320493a02d3077a538a1c796ad4488
lglglglgy <2661896437@qq.com>
2024-12-01 21:33:49 +0800

    Revert "格式"
    This reverts commit 7406628a
    
    Signed-off-by: lglglglgy <2661896437@qq.com>

--- Personal_Query.sql
+++ Personal_Query.sql
@@ -1,18 +1,7 @@
+DELIMITER //
 
 CREATE PROCEDURE QueryRecentMeal(IN user_id INT)
 BEGIN
-    -- 创建临时表
-    CREATE TEMPORARY TABLE IF NOT EXISTS TempRecentMeal (
-        UserID INT,
-        UserName VARCHAR(100),
-        DishID INT,
-        DishName VARCHAR(100),
-        Price DECIMAL(10, 2),
-        Quantity INT
-    );
-
-    -- 将查询结果插入临时表
-    INSERT INTO TempRecentMeal (UserID, UserName, DishID, DishName, Price, Quantity)
     SELECT
         buy.Bid AS UserID,
         buy.Bname AS UserName,
@@ -28,45 +17,26 @@ BEGIN
         buy.Bid = user_id
     ORDER BY
         bbv.Bid DESC
-    LIMIT 5;
-
-END;
--- bbv表根据传入的用户 ID 查询该用户最近的 5 条购买记录
-
-
-
-CREATE PROCEDURE GetRemainingMoney(IN user_id INT)
+    LIMIT 5; -- 最近一条购买记录
+END //
+--bbv表根据传入的用户 ID 查询该用户最近的 5 条购买记录
+CREATE PROCEDURE GetRemainingMoney(user_id INT)
 BEGIN
-    -- 创建临时表
-    CREATE TEMPORARY TABLE IF NOT EXISTS TempRemainingMoney (
-        UserID INT,
-        Money DECIMAL(10, 2)
-    );
 
-    -- 将查询结果插入临时表
-    INSERT INTO TempRemainingMoney (UserID, Money)
     SELECT money
     FROM buy
     WHERE Bid = user_id;
 
-    -- 选择临时表中的数据
-    SELECT * FROM TempRemainingMoney;
+END;//
 
-END;
-
-
-
-
-
-
-
-
+DELIMITER ;
+--buy表根据传入的用户 ID 查询并返回该用户的余额
 
 
 DELIMITER //
 
 
-CREATE FUNCTION GetTotalSpent(IN user_id INT) RETURNS DECIMAL(10, 2)
+CREATE FUNCTION GetTotalSpent(user_id INT) RETURNS DECIMAL(10, 2)
 DETERMINISTIC
 BEGIN
     DECLARE total_spent DECIMAL(10, 2);
@@ -76,7 +46,6 @@ BEGIN
     WHERE bbv.Bid = user_id;
     RETURN total_spent;
 END //
+
 DELIMITER ;
-
-
--- bbv表根据传入的用户 ID 查询并返回该用户的总消费金额，--DECIMAL(10, 2) 是一种数据类型，用于表示具有固定小数位数的精确数值。它通常用于存储货币或其他需要精确小数的数值。
+--bbv表根据传入的用户 ID 查询并返回该用户的总消费金额，--DECIMAL(10, 2) 是一种数据类型，用于表示具有固定小数位数的精确数值。它通常用于存储货币或其他需要精确小数的数值。
