862875de518f51196c61d0d5e459d0efe4c15d50
lglglglgy <2661896437@qq.com>
2024-12-04 08:58:26 +0800

    窗口查询
    Signed-off-by: lglglglgy <2661896437@qq.com>

--- Window_Menu.sql
+++ Window_Menu.sql
@@ -45,7 +45,7 @@ END //
 
 DELIMITER ;
 
---根据 win 表中的数据动态创建多个视图，每个视图包含与特定窗口相关的菜品及其对应的厨师信息。
+-- 根据 win 表中的数据动态创建多个视图，每个视图包含与特定窗口相关的菜品及其对应的厨师信息。
 DELIMITER //
 
 CREATE PROCEDURE QueryVegPurchases(IN dish_id INT)
@@ -69,55 +69,50 @@ BEGIN
 END //
 
 DELIMITER ;
---根据传入的菜品 ID 查询所有窗口的该菜品的所有购买记录，并返回相关信息，包括菜品 ID、菜品名称、用户 ID、用户名、购买数量和购买日期。通过调用这个存储过程，后端可以方便地获取指定菜品的购买记录。
+-- 根据传入的菜品 ID 查询所有窗口的该菜品的所有购买记录，并返回相关信息，包括菜品 ID、菜品名称、用户 ID、用户名、购买数量和购买日期。通过调用这个存储过程，后端可以方便地获取指定菜品的购买记录。
 
 
 DELIMITER //
 
-CREATE PROCEDURE CreateWindowVegPurchaseViews()
+CREATE PROCEDURE QueryWindowVegPurchases(
+    IN query_date DATE,
+    IN window_id INT
+)
 BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE current_wid INT;
-    DECLARE current_wadd INT;
-    DECLARE cur CURSOR FOR SELECT Wid, Wadd FROM win;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    -- 动态查询指定窗口和日期的数据
+    SET @query_sql = CONCAT(
+        'SELECT veg.Vid AS DishID, veg.Vname AS DishName, ',
+        'buy.Bid AS UserID, buy.Bname AS UserName, ',
+        'bbv.Bnum AS Quantity, bbv.Btime AS PurchaseDate ',
+        'FROM bbv ',
+        'INNER JOIN veg ON bbv.Vid = veg.Vid ',
+        'INNER JOIN buy ON bbv.Bid = buy.Bid ',
+        'INNER JOIN whv ON whv.Vid = veg.Vid ',
+        'WHERE whv.Wid = ', window_id, ' ',
+        'AND DATE(bbv.Btime) = "', query_date, '" ',
+        'ORDER BY bbv.Btime DESC'
+    );
 
-    -- 打开游标
-    OPEN cur;
+    -- 准备并执行查询
+    PREPARE stmt FROM @query_sql;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END //
 
-    fetch_loop: LOOP
-        FETCH cur INTO current_wid, current_wadd;
 
-        IF done THEN
-            LEAVE fetch_loop;
-        END IF;
 
-        -- 动态创建视图的 SQL
-        SET @view_name = CONCAT('window_', current_wadd, '_', current_wid, '_dish_purchases');
-        SET @create_view_sql = CONCAT(
-            'CREATE OR REPLACE VIEW ', @view_name, ' AS ',
-            'SELECT veg.Vid AS DishID, veg.Vname AS DishName, ',
-            'buy.Bid AS UserID, buy.Bname AS UserName, ',
-            'bbv.Bnum AS Quantity, bbv.Bdate AS PurchaseDate ',
-            'FROM bbv ',
-            'INNER JOIN veg ON bbv.Vid = veg.Vid ',
-            'INNER JOIN buy ON bbv.Bid = buy.Bid ',
-            'INNER JOIN whv ON whv.Vid = veg.Vid ',
-            'WHERE whv.Wid = ', current_wid, ' ',
-            'AND DATE(bbv.Bdate) = CURDATE() ', -- 限制为当天的记录
-            'ORDER BY bbv.Bdate DESC'
-        );
+DELIMITER ;
+-- 动态创建视图，每个视图包含某个窗口的每个菜品在同一天的购买记录
 
-        -- 执行动态 SQL 创建视图
-        PREPARE stmt FROM @create_view_sql;
-        EXECUTE stmt;
-        DEALLOCATE PREPARE stmt;
+DELIMITER //
 
-    END LOOP;
-
-    -- 关闭游标
-    CLOSE cur;
+CREATE PROCEDURE GetViewData(IN view_name VARCHAR(255))
+BEGIN
+    SET @query = CONCAT('SELECT * FROM ', view_name);
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 END //
 
 DELIMITER ;
---动态创建视图，每个视图包含某个窗口的每个菜品在同一天的购买记录
+
